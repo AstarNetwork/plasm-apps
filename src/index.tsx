11ce55f5a131b3d101c71b2fe973475a2790b7a3
t8m8 <t0m09488@gmail.com>
2020-04-07 02:26:02 +0900

    add entry point

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<></>, document.getElementById("root"));
