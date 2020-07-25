297360b25ef9741a91529e41275b507b89836e75
t8m8 <t0m09488@gmail.com>
2020-07-26 04:38:39 +0900

    show current era

--- src/PlasmInfo.tsx
+++ src/PlasmInfo.tsx
@@ -2,41 +2,54 @@ import React from "react";
 import styled from "styled-components";
 import { Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { EraIndex } from "@polkadot/types/interfaces";
+import { Option } from "@polkadot/types";
+import { useApi, useCall } from "@polkadot/react-hooks";
 
 import { Props } from "./types";
 import PlasmImage from "../public/plasm.png";
 import DustyImage from "../public/dusty.png";
 
 function PlasmInfo({ className }: Props): React.ReactElement {
+  const { api } = useApi();
+  const currentEra = useCall<Option<EraIndex>>(api.query.plasmRewards.currentEra, [])?.unwrap()?.toNumber() ?? 0;
   return (
     <div className={className}>
-      {process.env.TARGET === "dusty" ? (
-        <Image src={DustyImage} alt="dusty-network" className="logo" />
-      ) : (
-        <Image src={PlasmImage} alt="plasm-network" className="logo" />
-      )}
-      <span className="title">
-        <Link id="plasm-name" to="/">
-          {process.env.TARGET === "dusty" ? "Dusty Network" : "Plasm Network"}
-        </Link>
-      </span>
+      <div className="title">
+        {process.env.TARGET === "dusty" ? (
+          <Image src={DustyImage} alt="dusty-network" className="logo" />
+        ) : (
+          <Image src={PlasmImage} alt="plasm-network" className="logo" />
+        )}
+        <span className="name">
+          <Link id="plasm-name" to="/">
+            {process.env.TARGET === "dusty" ? "Dusty Network" : "Plasm Network"}
+          </Link>
+        </span>
+      </div>
+      <div className="info">
+        <span>Current Era: {currentEra}</span>
+      </div>
     </div>
   );
 }
 
 export default styled(PlasmInfo)`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
   margin-top: 1.5rem;
+
+  .title {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
 
   .logo {
     height: 30px;
     margin: 0.5rem;
   }
 
-  .title {
+  .name {
     display: table-cell;
     vertical-align: middle;
     width: 100%;
@@ -45,5 +58,12 @@ export default styled(PlasmInfo)`
   #plasm-name {
     color: #fff;
     white-space: nowrap;
+  }
+
+  .info {
+    font-size: 0.8rem;
+    color: #fff;
+    margin: 0 0.5rem;
+    text-align: right;
   }
 `;
